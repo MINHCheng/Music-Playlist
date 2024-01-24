15861776a7ac82b29cfed30001d1f9be2f80b36e
Your name <m22cheng@uwaterloo.ca>
2024-01-24 14:33:54 -0500

    lab 0 wed afternoon

--- driver.cpp
+++ driver.cpp
@@ -5,7 +5,14 @@
 
 
 int main(int argc, char *argv[]){
+    int x;
+    x = 6;
+    for (int i = 0; x != 15; i++)
+    {
+        x+=5;
+        std::cout << "Iteration number =" << i << std::endl;
+    }
     
-    // your code goes here...
+    std::cout <<"Done:\n";
 
 }
