86dde472b5bb5e1cce8b01550a540b98dbb41274
m22cheng <m22cheng@uwaterloo.ca>
2024-01-29 09:59:38 -0500

    "adding comments"

--- driver.cpp
+++ driver.cpp
@@ -9,27 +9,36 @@ int main()
     bool is_m = true;
     while (true)
     {
+        // i/p command from user which is either m,i,p,e and done
         string command;
         cin >> command;
+
+        // if done exits the code by returning
         if (command == "done")
         {
             myPlaylist.~Playlist();
             return 0;
         }
+
+        // if cmd = m, ask for a size and calls a function to createplaylist, make
         else if (command == "m"&&is_m)
         {
             int size;
             cin >> ws;
             cin >> size;
             cout << (myPlaylist.make(size));
-            is_m = false;
+            is_m = false; //if m is called, changes is_m to false so it cant be called again
         }
+
+        // if cmd = i, ask for a string and calls function to add a song to playlist
         else if (command == "i")
         {
             string t_a;
-            getline(cin,t_a);
+            getline(cin,t_a); //method to spaces within artist and song names
             cout << myPlaylist.add(t_a);
         }
+
+        // if cmd = p, ask for a position to play and calls play
         else if (command == "p")
         {
             int position;
@@ -37,6 +46,8 @@ int main()
             cin >> position;
             cout<<myPlaylist.play(position)<<endl;
         }
+
+        // if cmd is e, ask for a position to erase then calls remove function
         else if (command == "e")
         {
             int position;
@@ -44,11 +55,5 @@ int main()
             cin >> position;
             cout<<myPlaylist.remove(position)<<endl;
         }
-        else
-        {
-            cout<<"men\t";
-            // throw std::invalid_argument("please input a proper command");
-        }
     }
-    myPlaylist.~Playlist();
 }
--- playlist.cpp
+++ playlist.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 ////////////////////        Playlist           //////////////////////
 
+// default constructor to set all variables to default values
 Playlist::Playlist()
 {
     this->size = 0;
@@ -13,66 +14,74 @@ Playlist::Playlist()
     this->arr = nullptr;
 }
 
+// if method is called, creates of playlist of size m
 string Playlist::make(int size)
 {
-    this->size = size;
-    this->arr = new Songs[size];
+    this->size = size;           // obtains size which is arr.length
+    this->arr = new Songs[size]; // dynamically allocates array so it can update array during run time...allows to update as program progresses
     return "success\n";
 }
 
+// a method that first sees if new song meets conditions and if it does it creates new song, else it rejects and says no
 string Playlist::add(string t_a)
 {
     Songs newSong(t_a);
-    if (t_a == " Baby;Justin Beiber"|| (newSong.name == " My Heart Will Go On") || (this->numOfsongs == this->size))
+    // cannot equal Baby by Justin Beiber, MY Heart, and if the number of songs is equal to size then there is no space
+    if (t_a == " Baby;Justin Beiber" || (newSong.name == " My Heart Will Go On") || (this->numOfsongs == this->size))
     {
 
         return "can not insert" + t_a + "\n";
     }
+
+    // loops through all songs in array to see if it already added to the playlist
     for (unsigned int i = 0; i < this->size; i++)
     {
-        if (*(this->arr + i) == newSong)
+        if (*(this->arr + i) == newSong) // obtaining song within the array using pointer syntax and check if it is equal to the current song
         {
             return "can not insert" + t_a + "\n";
         }
     }
-    arr[this->numOfsongs++] = newSong;
+    arr[this->numOfsongs++] = newSong; // if clears all condition then adds the new song
     return "success\n";
 }
 
+// method to check if song is able to be played and if it is able to, it plays it; else it return cant play
 string Playlist::play(int position)
 {
-    if (position >= this->numOfsongs)
+    if (position >= this->numOfsongs) // if requested song is out of array size then rejects
     {
         return ("can not play " + to_string(position));
     }
     else
     {
-        return ("played " + to_string(position) + "" + arr[position].name + ";" + arr[position].artist);
+        return ("played " + to_string(position) + "" + arr[position].name + ";" + arr[position].artist); // plays the song
     }
 }
 
+// method to remove a song if it exist and shift everything up/down; else return a statement that says it can't
 string Playlist::remove(int position)
 {
-    if (numOfsongs <= position)
+    if (numOfsongs <= position) // if the position is greater or not in the domain of array size
     {
         return "can not erase " + to_string(position);
     }
     else
     {
-        int count = position + 1;
-        while (count < ((this->numOfsongs)))
+        int count = position + 1;            // takes position of the song that is above, so that it can be shifted down
+        while (count < ((this->numOfsongs))) // a conidtional to check if the counter is greater than the number of songs if not then loop
         {
-            arr[position++] = arr[count++];
+            arr[position++] = arr[count++]; // sets current array to the one above then increments both position and count after this lone executes
         }
         if (this->numOfsongs > 0)
         {
-            arr[(this->numOfsongs) - 1].reset();
+            arr[(this->numOfsongs) - 1].reset(); // resets the previous position of the top of the stack to default values
         }
-        --(this->numOfsongs);
+        --(this->numOfsongs); // decrements teh amount of songs in playlist
     }
     return "success";
 }
 
+// deallocates pointer array arr
 Playlist::~Playlist()
 {
     delete[] this->arr;
@@ -83,40 +92,44 @@ Playlist::~Playlist()
 
 ////////////////////              Songs              /////////////////////////////////
 
+// creates defualt contructor of default values
 Playlist::Songs::Songs() {}
 
+// tkaes a string with a ; in it and creates a song and artist with it
 Playlist::Songs::Songs(string t_a)
 {
-    string substring[2];
-    string tempstrng;
+    string substring[2]; // variable to store the artist and song name when string is split
+    string tempstrng;    // temp string to hold the split string
     int count = 0;
 
-    for (char ch : t_a)
+    for (char ch : t_a) // looping through every char in the string
     {
         if (ch == ';')
         {
-            substring[count++] = tempstrng;
-            tempstrng.clear();
+            substring[count++] = tempstrng; // if ';' is seen then assign the array the value seen then increments count so it can set the next value of array
+            tempstrng.clear();              // clears the temp string to create a new one oncse it sees a ';'
         }
         else
         {
             tempstrng += ch;
         }
     }
-    substring[count++] = tempstrng;
+    substring[count++] = tempstrng; //obtain the name of the song
     this->name = substring[0];
     this->artist = substring[1];
 }
 
+// a method to compare songs for add to see if song already exist
 bool Playlist::Songs::operator==(Songs current) const
 {
-    if (this->artist == current.artist && this->name == current.name)
+    if (this->artist == current.artist && this->name == current.name) //check if arist AND name are the same for both operands
     {
         return true;
     }
     return false;
 }
 
+// a method to set song to default values for the method of erase to reset last value in number of songs
 void Playlist::Songs::reset()
 {
     this->artist = "";
